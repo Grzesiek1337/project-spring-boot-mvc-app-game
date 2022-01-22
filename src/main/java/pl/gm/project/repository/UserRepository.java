f0bf73debe9abcae2bdf9c294884cda853fb3bc7
Grzesiek1337 <gmiedzian@gmail.com>
2022-01-22 14:49:12 +0100

    Changed crudrepository to jpa

--- src/main/java/pl/gm/project/repository/UserRepository.java
+++ src/main/java/pl/gm/project/repository/UserRepository.java
@@ -1,15 +1,12 @@
 package pl.gm.project.repository;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import pl.gm.project.model.User;
 
-
-
-
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.username = :username")
     User getUserByUsername(@Param("username") String username);
