33e00c10b8b76dda75d30b5b36e1770cccf1b5bb
Grzesiek1337 <gmiedzian@gmail.com>
2022-01-22 14:48:07 +0100

    Added item model class

---
+++ src/main/java/pl/gm/project/model/Item.java
@@ -0,0 +1,28 @@
+package pl.gm.project.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int increaseAttack;
+
+    private int increaseHealth;
+
+    private int increasePower;
+}
