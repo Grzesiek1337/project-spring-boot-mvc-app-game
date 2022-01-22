d4caf1b94df3b97facb9b5c9abbb11aa1a2540f1
Grzesiek1337 <gmiedzian@gmail.com>
2022-01-22 14:47:49 +0100

    Deleted unused method

--- src/main/java/pl/gm/project/service/HeroService.java
+++ src/main/java/pl/gm/project/service/HeroService.java
@@ -20,17 +20,8 @@ public class HeroService {
         return heroRepository.findAll();
     }
 
-    public List<Hero> listToChooseForUser() {
-        List<Hero> heroes = Arrays.asList(
-                new Hero(1L,"Barbarian",5,200,0,0,0,null),
-                new Hero(2L,"Assasin",10,150,0,0,0,null),
-                new Hero(3L,"Mage",15,100,0,0,0,null)
-        );
-        return heroes;
-    }
-
-    public void save(Hero Hero) {
-        heroRepository.save(Hero);
+    public void save(Hero hero) {
+        heroRepository.save(hero);
     }
 
     public Hero get(long id) {
