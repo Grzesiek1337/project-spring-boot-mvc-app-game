4b59ef1755fae92698b8631132d252ff0277a00b
Grzesiek1337 <gmiedzian@gmail.com>
2022-01-22 14:48:47 +0100

    Added item service class

---
+++ src/main/java/pl/gm/project/service/ItemService.java
@@ -0,0 +1,31 @@
+package pl.gm.project.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.gm.project.model.Item;
+import pl.gm.project.repository.ItemRepository;
+
+@Service
+@Transactional
+public class ItemService {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    public void save(Item item) {
+        itemRepository.save(item);
+    }
+
+    public Item get(long id) {
+        return itemRepository.findById(id).get();
+    }
+
+    public void update(Item item) {
+        itemRepository.save(item);
+    }
+
+    public void delete(long id) {
+        itemRepository.deleteById(id);
+    }
+}
