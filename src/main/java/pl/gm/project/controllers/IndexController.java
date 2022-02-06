895cce302c8616f518e7e740ccf2738e1064fc63
Grzesiek1337 <gmiedzian@gmail.com>
2022-02-06 19:57:03 +0100

    correcting code

--- src/main/java/pl/gm/project/controllers/IndexController.java
+++ src/main/java/pl/gm/project/controllers/IndexController.java
@@ -3,7 +3,6 @@ package pl.gm.project.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class IndexController {
 
