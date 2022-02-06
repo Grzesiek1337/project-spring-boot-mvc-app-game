39fe87f2c00547ae77100148fb94949e5ca6b778
Grzesiek1337 <gmiedzian@gmail.com>
2022-02-06 19:44:08 +0100

    added query finding item by name

--- src/main/java/pl/gm/project/repository/ItemRepository.java
+++ src/main/java/pl/gm/project/repository/ItemRepository.java
@@ -1,9 +1,12 @@
 package pl.gm.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.gm.project.model.Item;
-
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
+    @Query("SELECT i FROM Item i WHERE i.name = :name")
+    Item getItemByItemname(@Param("name") String name);
 }
