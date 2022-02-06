c5368d072198a0058c80826d90e67d1d1fae0a91
Grzesiek1337 <gmiedzian@gmail.com>
2022-02-06 20:15:02 +0100

    removed unused content

--- src/main/java/pl/gm/project/config/AppConfig.java
+++ src/main/java/pl/gm/project/config/AppConfig.java
@@ -4,11 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import pl.gm.project.converters.HeroConverter;
+
 import javax.validation.Validator;
 
 @Configuration
@@ -17,24 +16,9 @@ import javax.validation.Validator;
 @EnableJpaRepositories(basePackages = "pl.gm")
 public class AppConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(heroConverter());
-
-    }
-
-    @Bean
-    public HeroConverter heroConverter() {
-        return new HeroConverter();
-    }
-
     @Bean
     public Validator validator() {
         return new LocalValidatorFactoryBean();
     }
-
-
-
-
 
 }
