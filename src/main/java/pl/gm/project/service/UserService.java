e512c6c8788e81230f4578309f16fb584433399a
Grzesiek1337 <gmiedzian@gmail.com>
2022-02-11 19:46:29 +0100

    method have been transfered from controller to service

--- src/main/java/pl/gm/project/service/UserService.java
+++ src/main/java/pl/gm/project/service/UserService.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import pl.gm.project.model.User;
 import pl.gm.project.repository.UserRepository;
 
@@ -26,12 +28,22 @@ public class UserService {
         userRepository.save(user);
     }
 
-    public void saveNewUser(User user) {
+    public String checkAndSaveUser(User user, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "usercontent/user/user_create";
+        }
+        if (userRepository.getUserByUsername(user.getUsername()) != null) {
+            model.addAttribute("msg", "User already exist.");
+            return "usercontent/user/user_create";
+        }
+        model.addAttribute("successCreatingAccountMsg", "U have successfully created your account! Log and play!");
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         user.setEnabled(true);
         user.setRole("ROLE_USER");
         userRepository.save(user);
+        return "index";
     }
+
     public void update(User user) {
         userRepository.save(user);
     }
