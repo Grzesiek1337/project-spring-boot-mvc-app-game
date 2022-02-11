87d435061aa447d4210c1b12f9a9b0726bab7e4d
Grzesiek1337 <gmiedzian@gmail.com>
2022-02-11 19:24:18 +0100

    correcting code

--- src/main/java/pl/gm/project/service/UserDetailsServiceImpl.java
+++ src/main/java/pl/gm/project/service/UserDetailsServiceImpl.java
@@ -1,7 +1,5 @@
 package pl.gm.project.service;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
