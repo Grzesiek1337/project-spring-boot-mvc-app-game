21f4b6517139ed7fa02d8233f9eeab3447754d15
Grzesiek1337 <gmiedzian@gmail.com>
2022-02-17 12:25:25 +0100

    changed endpoints in annotations,method names,removed 2 unused methods

--- src/main/java/pl/gm/project/controllers/admincontrollers/AdminPanelController.java
+++ src/main/java/pl/gm/project/controllers/admincontrollers/AdminPanelController.java
@@ -18,15 +18,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AdminPanelController {
 
-
     private final HeroService heroService;
     private final UserService userService;
     private final ItemService itemService;
     private final MobService mobService;
 
     @ModelAttribute("user")
-    public CurrentUserDetails getUser(@AuthenticationPrincipal CurrentUserDetails currentUserDetails) {
-        return currentUserDetails;
+    public User getUser(@AuthenticationPrincipal CurrentUserDetails currentUserDetails) {
+        return userService.get(currentUserDetails.getUser().getId());
     }
 
     @ModelAttribute("localDateTime")
@@ -39,114 +38,103 @@ public class AdminPanelController {
         return "admincontent/admin_panel";
     }
 
-    @GetMapping("/hero_list")
+    @GetMapping("/hero/list")
     public String viewListOfHeroes(Model model) {
         List<Hero> heroes = heroService.listAll();
         model.addAttribute("heroList", heroes);
         return "admincontent/hero/hero_list";
     }
-    @GetMapping("/hero_new")
-    public String showNewHeroPage(Model model) {
-        model.addAttribute("hero", new Hero());
-        return "admincontent/hero/hero_new";
-    }
 
-    @PostMapping("/hero_new")
-    public String saveHero(@ModelAttribute("hero") Hero hero) {
-        heroService.save(hero);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/hero_edit/{id}")
-    public String showEditHeroPageForAdmin(@PathVariable(name = "id") int id, Model model) {
+    @GetMapping("/hero/edit/{id}")
+    public String showEditHeroFormForAdmin(@PathVariable(name = "id") int id, Model model) {
         Hero hero = heroService.get(id);
         model.addAttribute("hero", hero);
         return "admincontent/hero/hero_edit";
     }
 
-    @PostMapping("/hero_edit")
-    public String updateHeroForAdmin(Hero hero) {
+    @PostMapping("/hero/edit")
+    public String updateHeroByAdmin(Hero hero) {
         heroService.update(hero);
         return "redirect:/admin";
     }
-    @GetMapping("/hero_remove/{id}")
+    @GetMapping("/hero/remove/{id}")
     public String getRemoveFormForHero(@PathVariable int id, Model model) {
         model.addAttribute("heroId", id);
         return "admincontent/hero/hero_delete";
     }
 
-    @GetMapping("/hero_delete/{id}")
-    public String removeHero(@PathVariable long id, Model model) {
+    @GetMapping("/hero/delete/{id}")
+    public String removeHeroByAdmin(@PathVariable long id, Model model) {
         heroService.delete(id);
         model.addAttribute("heroId", null);
-        return "redirect:/admin/hero_list";
+        return "redirect:/admin/hero/list";
     }
 
-    @GetMapping("/user_list")
-    public String viewListOfUsers(Model model) {
+    @GetMapping("/user/list")
+    public String viewListOfUsersForAdmin(Model model) {
         Iterable<User> users = userService.listAll();
         model.addAttribute("userList", users);
         return "admincontent/user/user_list";
     }
 
-    @GetMapping("/user_new")
-    public String showNewUserPage(Model model) {
+    @GetMapping("/user/new")
+    public String showNewUserFormForAdmin(Model model) {
         model.addAttribute("userNew", new User());
         return "admincontent/user/user_new";
     }
 
-    @PostMapping("/user_new")
-    public String saveUser(@ModelAttribute("userNew") User user) {
-        userService.save(user);
+    @PostMapping("/user/new")
+    public String saveUserByAdmin(@ModelAttribute("userNew") User user) {
+        userService.saveUserByAdmin(user);
         return "redirect:/admin";
     }
 
-    @GetMapping("/user_remove/{id}")
-    public String getRemoveFormForUSer(@PathVariable int id, Model model) {
+    @GetMapping("/user/remove/{id}")
+    public String getUserRemoveFormForAdmin(@PathVariable int id, Model model) {
         model.addAttribute("userId", id);
         return "admincontent/user/user_delete";
     }
-    @GetMapping("/user_delete/{id}")
-    public String removeUser(@PathVariable long id, Model model) {
+    @GetMapping("/user/delete/{id}")
+    public String removeUserByAdmin(@PathVariable long id, Model model) {
         userService.delete(id);
         model.addAttribute("userId", null);
-        return "redirect:/admin/user_list";
+        return "redirect:/admin/user/list";
     }
 
-    @GetMapping("/item_list")
-    public String viewListOfItems(Model model) {
+    @GetMapping("/item/list")
+    public String viewListOfItemsForAdmin(Model model) {
         Iterable<Item> items = itemService.listAll();
         model.addAttribute("itemList", items);
         return "admincontent/item/item_list";
     }
 
-    @GetMapping("/item_new")
-    public String showNewItemPage(Model model) {
+    @GetMapping("/item/new")
+    public String showNewItemFormForAdmin(Model model) {
         model.addAttribute("item", new Item());
         return "admincontent/item/item_new";
     }
 
-    @PostMapping("/item_new")
-    public String saveItem(@ModelAttribute("item") Item item) {
+    @PostMapping("/item/new")
+    public String saveItemByAdminToDatabase(@ModelAttribute("item") Item item) {
         itemService.save(item);
         return "redirect:/admin";
     }
 
-    @GetMapping("/mob_list")
-    public String viewListOfMobs(Model model) {
+    @GetMapping("/mob/list")
+    public String viewListOfMobsForAdmin(Model model) {
         List<Mob> mobs = mobService.listAll();
         model.addAttribute("mobList", mobs);
         return "admincontent/mob/mob_list";
     }
 
-    @GetMapping("/mob_new")
-    public String showNewMobPage(Model model) {
+    @GetMapping("/mob/new")
+    public String showNewMobFormForAdmin(Model model) {
         model.addAttribute("mob", new Mob());
         return "admincontent/mob/mob_new";
     }
 
-    @PostMapping("/mob_new")
-    public String saveMob(@ModelAttribute("mob") Mob mob) {
+    @PostMapping("/mob/new")
+    public String saveMobByAdminToDatabase(@ModelAttribute("mob") Mob mob) {
         mobService.save(mob);
         return "redirect:/admin";
     }
