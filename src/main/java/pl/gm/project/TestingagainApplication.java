10a6a42a8c298843f29ca84e0cd84a1f9beaf3dd
Grzesiek1337 <gmiedzian@gmail.com>
2022-06-02 20:58:49 +0200

    test commit

--- src/main/java/pl/gm/project/TestingagainApplication.java
+++ src/main/java/pl/gm/project/TestingagainApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestingagainApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TestingagainApplication.class, args);
     }
